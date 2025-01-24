d83ef1788f95b231c763c6125bcf53846319c74f
dtcherniak <dima.tcherniak@gmail.com>
2025-01-24 09:01:25 +0000

    Working version

--- cpsns_LifePlot_v2.py
+++ cpsns_LifePlot_v2.py
@@ -13,6 +13,7 @@ import struct
 import time
 import argparse
 import json
+import sys
 
 HOST_DEFAULT = "dtl-server-2.st.lab.au.dk"
 PORT_DEFAULT = 8090
@@ -67,22 +68,41 @@ def on_message(client, userdata, msg):
             json_metadata = json.loads(msg.payload)
             nSamples = json_metadata['Data']['Samples']
             cType = json_metadata['Data']['Type'][0]
-            Fs = json_metadata["Analysis chain"][0]["Sampling"]
+            Fs = json_metadata["Analysis chain"][-1]["Sampling"] # take the sampling freq. from the last element of the analysis chain!
             # Need to know: physical quantity, units
             strPhysQuantity = json_metadata["Analysis chain"][-1]["Output"]
             strUnits = json_metadata["Data"]["Unit"]
-            secAtAcqusitionStart = 0
+            # Only for metadata ver.>=2
+            try:
+                secAtAcqusitionStart = json_metadata["TimeAtAquisitionStart"]["Seconds"]
+                nanosec = json_metadata["TimeAtAquisitionStart"]["Nanosec"]
+            except Exception as e:
+                print("Incompatible version. Use earlier version of cpsns_LifePlot!", file=sys.stderr)
+            
             usTimeStamp = 0
             #                0         1      2   3                4         5              6     7                     8
-            myDict[myKey] = [nSamples, cType, Fs, strPhysQuantity, strUnits, queue.Queue(), None, secAtAcqusitionStart, usTimeStamp]
+            #myDict[myKey] = [nSamples, cType, Fs, strPhysQuantity, strUnits, queue.Queue(), None, secAtAcqusitionStart, usTimeStamp]
+            myDict[myKey] = {
+                "SamplesInPayload": nSamples, 
+                "DataType": cType, 
+                "SampleRate": Fs, 
+                "PhysicalQuantity": strPhysQuantity, 
+                "Units": strUnits, 
+                "PayloadQueue": queue.Queue(), 
+                "SecAtAcqusiitionStart": secAtAcqusitionStart, 
+                "Nanosec": nanosec,
+                "SampleWhenEntryCreated": 0,
+                "Data": None,
+                "PlotLine": None
+            }
     else:
         if myKey in myDict:
             # Parse the payload
             payload = msg.payload
             descriptorLength, metadataVer = struct.unpack_from('HH', payload)
             # how many samples and what's its type, float or double?
-            cType = myDict[myKey][1]
-            nSamples = myDict[myKey][0]
+            cType = myDict[myKey]["DataType"]
+            nSamples = myDict[myKey]["SamplesInPayload"]
             if nSamples == -1: # unknown or variable
                 # calculate nSamples from the payload length
                 payload_len = len(payload)
@@ -91,27 +111,20 @@ def on_message(client, userdata, msg):
             strBinFormat = str(nSamples) + str(cType)  # e.g., '640f' for 640 floats
             # data
             data = np.array(struct.unpack_from(strBinFormat, payload, descriptorLength))
-            # time stamp
+            # time stamp of the payload
             secFromEpoch = struct.unpack_from('Q', payload, 4)[0]
             nanosec = struct.unpack_from('Q', payload, 12)[0]
             # nSamples
             nSamplesFromDAQStart = 0
             if metadataVer >= 2:
                 nSamplesFromDAQStart = struct.unpack_from('Q', payload, 20)[0]
+            else:
+                raise Exception("Incompatible version. Use earlier version of cpsns_LifePlot!")
+            if myDict[myKey]["SampleWhenEntryCreated"] == 0:
+                # First time
+                myDict[myKey]["SampleWhenEntryCreated"] = nSamplesFromDAQStart
 
-            if myDict[myKey][7] == 0:
-                myDict[myKey][7] = secFromEpoch
-
-            # Adjust to be inside the DOUBLE resolution
-            secFromAcquisitionStart = secFromEpoch - myDict[myKey][7]
-            # Time stamp is the time in microsec (us) from the data acqusition start
-            usTimeStamp = secFromAcquisitionStart * 1e6 + nanosec * 1e-3  # e(-9+6)
-            # Dima 18-Dec
-            print(f"Current time: {usTimeStamp/1000000} s")
-            # end Dima 18-Dec
-            myDict[myKey][8] = usTimeStamp
-            # Update the dictionary
-            myDict[myKey][5].put(data)
+            myDict[myKey]["PayloadQueue"].put({"SampleFromDAQStart": nSamplesFromDAQStart, "PayloadData": data})
         else:
             print("Waiting for the metadata...")
     
@@ -153,8 +166,12 @@ def main():
     # Labels
     ax.set_xlabel('Time, s')
 
+    # common time axis
+    tAxisCommon = None
+
     last_redraw_time = time.time()
     while True:
+        bNeedToReset = False
         # Dima 18-Dec
         current_time = time.time()
         if current_time-last_redraw_time >= 0.1:
@@ -169,84 +186,92 @@ def main():
                 # print("Waiting for bWritingMyDict")
                 time.sleep(0.0001)
             bReadingMyDict = True    
-
+            
             for key, val in myDict.items():
-                if val[5].empty():
+                if val["PayloadQueue"].empty():
                     continue
 
-                data = None
-                while not val[5].empty():
-                    if data is None:
-                        data = val[5].get()
+                # Define the global time axis
+                if tAxisCommon is None:
+                    TimeToCover = 3 # seconds
+                    tAxisCommon = {"Fs": val["SampleRate"], "AxisStartsAtSample": val["SampleWhenEntryCreated"], "AxisLength": int(round(TimeToCover * val["SampleRate"]))}
+                    #print(f'AxisLength = {tAxisCommon["AxisLength"]}')
+
+                if val["Data"] is None:
+                    # allocate
+                    val["Data"] = np.full(tAxisCommon["AxisLength"], 0, dtype=np.float32)
+
+                while not val["PayloadQueue"].empty():
+                    # place the data where it is suposed to be
+                    payload = val["PayloadQueue"].get()
+                    inx = payload["SampleFromDAQStart"]
+                    inxToPlaceData = inx - tAxisCommon["AxisStartsAtSample"]
+                    if inxToPlaceData<0:
+                        ## ignore this payload...
+                        #print(f"key={key}. Negative index: {inxToPlaceData}. The payload is ignored!")  
+                        #continue
+                        # need to reset!
+                        bNeedToReset = True
+                        break
+
+                    data = payload["PayloadData"]
+                    if inxToPlaceData+len(data) < tAxisCommon["AxisLength"]:
+                        # okay
+                        val["Data"][inxToPlaceData:inxToPlaceData+len(data)] = data
                     else:
-                        data = np.hstack((data, val[5].get()))
+                        # need to "scroll"... by 
+                        scrl = 1 + inxToPlaceData+len(data) - tAxisCommon["AxisLength"]
+                        #print(f"key={key}. Scrolling by {scrl} elements")  
+                        # scroll the time axis
+                        tAxisCommon["AxisStartsAtSample"] += scrl
+                        if scrl < tAxisCommon["AxisLength"]:
+                            # scroll all other dataset
+                            for kk in myDict:
+                                if myDict[kk]["Data"] is None:
+                                    continue
+                                myDict[kk]["Data"] = np.roll(myDict[kk]["Data"], -scrl)
+                                if kk == key:
+                                    myDict[kk]["Data"][inxToPlaceData-scrl:inxToPlaceData+len(data)-scrl] = data
+                                else:
+                                    myDict[kk]["Data"][-scrl:] = np.full(scrl, 0, dtype=np.float32)                                                
+                        else:
+                            # need to reset!
+                            bNeedToReset = True
+                            break
 
-                print(data.shape)
-                nPackSize = len(data)
-                if val[6] is None:
-                    Fs = val[2]
-                    lng = 1000
-                    timeAxis = np.linspace(0, lng * nPackSize / Fs, lng * nPackSize)
-                    line, = ax.plot(timeAxis, np.zeros(lng * nPackSize), label=str(key))
+                # add a line if it is not here
+                if val["PlotLine"] is None:
+                    ta = np.linspace(0, tAxisCommon["AxisLength"]/tAxisCommon["Fs"], tAxisCommon["AxisLength"])
+                    line, = ax.plot(ta, val["Data"], label=str(key))
                     plt.legend(loc="upper left")
-                    val[6] = line
+                    val["PlotLine"] = line
 
-                line = val[6]
-                curData = line.get_ydata()
-                curData = np.roll(curData, -nPackSize)
-                curData[-nPackSize:] = data
-                line.set_ydata(curData)
-                #bNeedToRedraw = True # Dima 16-Dec
+                line = val["PlotLine"]
+                line.set_ydata(val["Data"])
 
-            fig.canvas.draw()
-            fig.canvas.flush_events()
+            if bNeedToReset:
+                print("Long break in the data detected. Resetting the plot", file=sys.stderr)
+                bNeedToReset = False
+                # remove the lines
+                for kk in myDict:
+                    if myDict[kk]["PlotLine"] is not None:
+                        myDict[kk]["PlotLine"].remove()
+
+                # reset the dictionary
+                myDict = {}
+                tAxisCommon = None
+
+                # remove the legend
+                plt.legend().remove()
+                plt.show()
 
             bReadingMyDict = False
 
-        time.sleep(0.1) # Dima 16-Dec
-
-    """
-    # initiate the plt
-    plt.ion()
-    fig, ax = plt.subplots()
-    timeAxis = np.linspace(0, 3 * nPackSize / Fs, 3 * nPackSize)
-    line1, = ax.plot(timeAxis, np.zeros(3 * nPackSize))
-    line2, = ax.plot(timeAxis, np.zeros(3 * nPackSize))
-    # New: grid lines
-    ax.grid(True)
-    # Labels
-    ax.set_xlabel('Time, s')
-    ax.set_ylabel('Acceleration, m/s^2')
-    # Limits
-    plt.ylim(-2, 2)
-
-    while True:
-        bNeedToRedraw = False
-        if not data_queue_ch1.empty():
-            data = data_queue_ch1.get()
-            # line1.set_xdata(np.linspace(timeStamp_ch1,timeStamp_ch1 + 1e6*nPackSize/Fs ,nPackSize)) # timeStamp_ch1 in us
-            # current data
-            curData = line1.get_ydata()
-            curData = np.roll(curData, -nPackSize)
-            curData[-nPackSize:] = data
-            line1.set_ydata(curData)
-            bNeedToRedraw = True
-        if not data_queue_ch2.empty():
-            data = data_queue_ch2.get()
-            # line2.set_xdata(np.linspace(timeStamp_ch2,timeStamp_ch2 + 1e6*nPackSize/Fs ,nPackSize))
-            curData = line2.get_ydata()
-            curData = np.roll(curData, -nPackSize)
-            curData[-nPackSize:]=data
-            line2.set_ydata(curData)
-            bNeedToRedraw = True
-        if bNeedToRedraw:
             fig.canvas.draw()
             fig.canvas.flush_events()
 
-        # Sleep for a short time to reduce CPU usage
-        time.sleep(0.1)
-        pass
-    """
+        time.sleep(0.1) # Dima 16-Dec
+
 
 if __name__ == "__main__":
     main()
